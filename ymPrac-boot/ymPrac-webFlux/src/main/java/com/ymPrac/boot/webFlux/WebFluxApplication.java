0c301358bfc10814ad3745b0d32e05576e3a4348
yanmeng <jnyanmeng@163.com>
2018-08-06 22:36:54 +0800

    fix

---
+++ ymPrac-boot/ymPrac-webFlux/src/main/java/com/ymPrac/boot/webFlux/WebFluxApplication.java
@@ -0,0 +1,16 @@
+package com.ymPrac.boot.webFlux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yan Meng
+ * @date 2018/8/6.
+ */
+@SpringBootApplication
+public class WebFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxApplication.class, args);
+    }
+}
